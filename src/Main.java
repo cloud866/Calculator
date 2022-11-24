e3bca45b3be9f2e93e995ac9407002138c18898c
cloud866 <dklein927@gmail.com>
2022-11-24 18:06:43 +0200

    SomeX

--- src/Main.java
+++ src/Main.java
@@ -8,17 +8,16 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter first number");
         int operand1 = scanner.nextInt(); //считывает число
+        System.out.println("Choose an operator: +, -, *, or /");
+        char operation = scanner.next().charAt(0);
         System.out.println("Enter second number");
         int operand2 = scanner.nextInt();
 /*Метод next() класса Scanner считывает строку,
 а метод charAt(0) позволяет взять первый символ в этой строке.
  так мы получаем операцию, которую нужно выполнить*/
-        System.out.println("Choose an operator: +, -, *, or /");
-        char operation = scanner.next().charAt(0);
 
         switch (operation) {
 
-            // performs addition between numbers
             case '+':
                 result = operand1 + operand2;
                 System.out.println(operand1 + " + " + operand2 + " = " + result);
